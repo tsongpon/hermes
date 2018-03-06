aef5162a9dcef561808e161709cfe17c02327902
Songpon Imyen <t.songpon@gmail.com>
2018-03-06 18:02:14 +0700

    [skip ci] add test delete driendpick

--- src/test/kotlin/asia/vmdigital/hermes/web/v1/controller/FriendPickApiIntegrationTest.kt
+++ src/test/kotlin/asia/vmdigital/hermes/web/v1/controller/FriendPickApiIntegrationTest.kt
@@ -4,6 +4,7 @@ import com.github.kristofa.test.http.Method
 import com.github.kristofa.test.http.MockHttpServer
 import com.github.kristofa.test.http.SimpleHttpResponseProvider
 import com.jayway.restassured.RestAssured
+import com.jayway.restassured.RestAssured.`when`
 import com.jayway.restassured.module.jsv.JsonSchemaValidator
 import org.apache.http.HttpStatus
 import org.hamcrest.CoreMatchers
@@ -40,6 +41,151 @@ class FriendPickApiIntegrationTest {
     @After
     fun cleanUp() {
         server!!.stop()
+    }
+
+    @Test
+    fun testGetFriendPickOneFriendSave() {
+        val contractOwnerResponseUser02 = """
+            {
+                "result": [
+                    {
+                        "id": "user01",
+                        "friendly_id": "user1",
+                        "friendly_id_updated": false,
+                        "profile_name": "user01",
+                        "profile_photo": "bphoto",
+                        "email": "user1@vmd.asia",
+                        "mood": "",
+                        "mood_expired_at": null,
+                        "onboarded": false,
+                        "require_verification": true,
+                        "email_verified": false,
+                        "language": "en-US"
+                    }
+                ]
+            }
+            """.trimIndent()
+
+        val userInfoResponseUser02 = """
+            {
+                "id": "user02",
+                "friendly_id": "user02",
+                "friendly_id_updated": false,
+                "profile_name": "user02",
+                "profile_photo": "bphoto",
+                "email": "user02@vmd.asia",
+                "mood": "",
+                "mood_expired_at": null,
+                "onboarded": false,
+                "require_verification": true,
+                "email_verified": false,
+                "language": "en-US"
+            }
+            """.trimIndent()
+        responseProvider!!.expect(Method.GET, "/accounts/v1/users/user02/contactowners")
+                .respondWith(200,
+                        "application/json", contractOwnerResponseUser02)
+
+        responseProvider!!.expect(Method.GET, "/accounts/v1/users/user02")
+                .respondWith(200,
+                        "application/json", userInfoResponseUser02)
+
+        val requestJson = """
+            {
+	            "user_id": "user02",
+                "place_id": "user02-place-01",
+	            "type": "Google",
+	            "categories": ["restaurant"],
+	            "lon": 100.4534,
+	            "lat": 14.0525
+            }
+        """.trimIndent()
+
+        RestAssured.given().contentType("application/json").body(requestJson)
+                .`when`().post("saved/v1/user/user02/saved")
+                .then().statusCode(HttpStatus.SC_OK).log().body()
+                .extract().path<String>("id")
+
+        RestAssured.`when`().get("friendpicks/v1/user/user01/friendpicks")
+                .then().statusCode(HttpStatus.SC_OK).log().body()
+                .and().body(JsonSchemaValidator.matchesJsonSchemaInClasspath(
+                        "json-schema/friendpickResponseSchema.json"))
+                .and().body("result[0].user_id", equalTo("user01"))
+                .and().body("result[0].place_id", equalTo("user02-place-01"))
+                .and().body("result[0].type", equalTo("Google"))
+                .and().body("result[0].friends[0].friend_id", equalTo("user02"))
+    }
+
+    @Test
+    fun testDelete() {
+        val contractOwnerResponseUser02D = """
+            {
+                "result": [
+                    {
+                        "id": "user01d",
+                        "friendly_id": "user1D",
+                        "friendly_id_updated": false,
+                        "profile_name": "user01D",
+                        "profile_photo": "bphoto",
+                        "email": "user1D@vmd.asia",
+                        "mood": "",
+                        "mood_expired_at": null,
+                        "onboarded": false,
+                        "require_verification": true,
+                        "email_verified": false,
+                        "language": "en-US"
+                    }
+                ]
+            }
+            """.trimIndent()
+
+        val userInfoResponseUser02d = """
+            {
+                "id": "user02d",
+                "friendly_id": "user02D",
+                "friendly_id_updated": false,
+                "profile_name": "user02D",
+                "profile_photo": "bphoto",
+                "email": "user02D@vmd.asia",
+                "mood": "",
+                "mood_expired_at": null,
+                "onboarded": false,
+                "require_verification": true,
+                "email_verified": false,
+                "language": "en-US"
+            }
+            """.trimIndent()
+        responseProvider!!.expect(Method.GET, "/accounts/v1/users/user02d/contactowners")
+                .respondWith(200,
+                        "application/json", contractOwnerResponseUser02D)
+
+        responseProvider!!.expect(Method.GET, "/accounts/v1/users/user02d")
+                .respondWith(200,
+                        "application/json", userInfoResponseUser02d)
+
+        val requestJson = """
+            {
+	            "user_id": "user02d",
+                "place_id": "user02-place-01d",
+	            "type": "Google",
+	            "categories": ["restaurant"],
+	            "lon": 10.4534,
+	            "lat": 1.0525
+            }
+        """.trimIndent()
+
+        RestAssured.given().contentType("application/json").body(requestJson)
+                .`when`().post("saved/v1/user/user02d/saved")
+                .then().statusCode(HttpStatus.SC_OK).log().body()
+                .extract().path<String>("id")
+
+        val id = RestAssured.`when`().get("friendpicks/v1/user/user01d/friendpicks")
+                .then().statusCode(HttpStatus.SC_OK).log().body().extract().path<String>("result[0].id")
+
+        `when`().delete("friendpicks/v1/user/user01d/friendpicks/$id").then().statusCode(HttpStatus.SC_OK)
+
+        `when`().get("friendpicks/v1/user/user01d/friendpicks")
+                .then().statusCode(HttpStatus.SC_OK).and().body("result.size()", CoreMatchers.`is`(0))
     }
 
     @Test
@@ -176,78 +322,5 @@ class FriendPickApiIntegrationTest {
                 .and().body("result[0].friends.size()", CoreMatchers.`is`(2))
                 .and().body("result[0].friends[0].friend_id", equalTo("userFp03")) //save after come first
                 .and().body("result[0].friends[1].friend_id", equalTo("userFp02")) //save fist come after
-    }
-
-    @Test
-    fun testGetFriendPickOneFriendSave() {
-        val contractOwnerResponseUser02 = """
-            {
-                "result": [
-                    {
-                        "id": "user01",
-                        "friendly_id": "user1",
-                        "friendly_id_updated": false,
-                        "profile_name": "user01",
-                        "profile_photo": "bphoto",
-                        "email": "user1@vmd.asia",
-                        "mood": "",
-                        "mood_expired_at": null,
-                        "onboarded": false,
-                        "require_verification": true,
-                        "email_verified": false,
-                        "language": "en-US"
-                    }
-                ]
-            }
-            """.trimIndent()
-
-        val userInfoResponseUser02 = """
-            {
-                "id": "user02",
-                "friendly_id": "user02",
-                "friendly_id_updated": false,
-                "profile_name": "user02",
-                "profile_photo": "bphoto",
-                "email": "user02@vmd.asia",
-                "mood": "",
-                "mood_expired_at": null,
-                "onboarded": false,
-                "require_verification": true,
-                "email_verified": false,
-                "language": "en-US"
-            }
-            """.trimIndent()
-        responseProvider!!.expect(Method.GET, "/accounts/v1/users/user02/contactowners")
-                .respondWith(200,
-                        "application/json", contractOwnerResponseUser02)
-
-        responseProvider!!.expect(Method.GET, "/accounts/v1/users/user02")
-                .respondWith(200,
-                        "application/json", userInfoResponseUser02)
-
-        val requestJson = """
-            {
-	            "user_id": "user02",
-                "place_id": "user02-place-01",
-	            "type": "Google",
-	            "categories": ["restaurant"],
-	            "lon": 100.4534,
-	            "lat": 14.0525
-            }
-        """.trimIndent()
-
-        RestAssured.given().contentType("application/json").body(requestJson)
-                .`when`().post("saved/v1/user/user02/saved")
-                .then().statusCode(HttpStatus.SC_OK).log().body()
-                .extract().path<String>("id")
-
-        RestAssured.`when`().get("friendpicks/v1/user/user01/friendpicks")
-                .then().statusCode(HttpStatus.SC_OK).log().body()
-                .and().body(JsonSchemaValidator.matchesJsonSchemaInClasspath(
-                        "json-schema/friendpickResponseSchema.json"))
-                .and().body("result[0].user_id", equalTo("user01"))
-                .and().body("result[0].place_id", equalTo("user02-place-01"))
-                .and().body("result[0].type", equalTo("Google"))
-                .and().body("result[0].friends[0].friend_id", equalTo("user02"))
     }
 }
